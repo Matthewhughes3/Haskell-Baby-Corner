933cff1dbef23ae489f56250aa3e0fbcabafa2d9
Matthew Hughes <matthewhughes@pointstire.com>
2020-03-18 20:32:07 -0700

    put way too much effort into a dumb joke

--- numberTheory.hs
+++ numberTheory.hs
@@ -12,5 +12,11 @@ howSmartMeAndMyBrotherAre = howSmartIAm ++ " " ++ howSmartMyBrotherIs
 
 mineAndMyBrothersFavoriteNumbersWithFiveAtTheBeginning = 5:mineAndMyBrothersFavoriteNumbers
 
--- TODO: figure out how to explicitly type favoriteNumbers as a list of numbers
+-- TODO: figure out how to explicitly type favoriteNumbers as a list of ints
 someOnesSecondFavoriteNumber favoriteNumbers = favoriteNumbers !! 1
+
+-- TODO: figure out how to explicitly type numberThatIsQuitePossiblyBetweenOneAndTwentyButNotDefinitely as an int
+isBetweenOneAndTwenty numberThatIsQuitePossiblyBetweenOneAndTwentyButNotDefinitely = numberThatIsQuitePossiblyBetweenOneAndTwentyButNotDefinitely `elem` [1..20]
+
+-- TODO: get a wider monitor
+evenNumbersInBetween numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers myFingersHurt = if numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers `mod` 2 == 0 then [numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers, numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers + 2..myFingersHurt] else [numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers + 1, numberThatYouWouldLikeToUseToBeginYourConquestOfEvenNumbers + 3..myFingersHurt]
