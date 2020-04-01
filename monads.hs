f35d564f4ca1db7ad65ad25c86e4a876ce7db61b
Matthew Hughes <matthew.hughes71499@gmail.com>
2020-04-01 08:40:54 -0700

    What I learned morning of 4/1

---
+++ monads.hs
@@ -0,0 +1,33 @@
+import Control.Monad
+
+--monadFilter :: (a -> Bool) -> [a] -> [a]
+--monadFilter f xs = xs >>= (\x -> guard (f x) >> return x)
+
+monadFilter :: (a -> Bool) -> [a] -> [a]
+monadFilter f xs = do
+  x <- xs
+  guard (f x)
+  return x
+
+type KnightPos = (Int,Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do
+  (c',r') <- [
+              (c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1),
+              (c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
+             ]
+  guard (c' `elem` [1..8] && r' `elem` [1..8])
+  return (c',r')
+
+--in3 :: KnightPos -> [KnightPos]
+--in3 start = do
+--  first <- moveKnight start
+--  second <- moveKnight first
+--  moveKnight second
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
---
+++ writer.hs
@@ -0,0 +1,34 @@
+newtype Writer w a = Writer { runWriter :: (a,w) } deriving (Show)
+
+instance Functor (Writer w) where
+  fmap f (Writer (x,v)) = Writer (f x,v)
+
+instance (Monoid w) => Applicative (Writer w) where
+  pure x = Writer (x, mempty)
+  Writer (f,v) <*> Writer (x,v') = Writer (f x, v `mappend` v')
+
+instance (Monoid w) => Monad (Writer w) where  
+  return x = Writer (x, mempty)  
+  (Writer (x,v)) >>= f = let (Writer (y, v')) = f x in Writer (y, v `mappend` v')  
+
+logNumber :: Int -> Writer [String] Int  
+logNumber x = Writer (x, ["Got number: " ++ show x])
+
+tell :: (Monoid a) => a -> Writer a ()
+tell v = Writer ((), v)
+  
+multWithLog :: Writer [String] Int  
+multWithLog = do  
+  a <- logNumber 3  
+  b <- logNumber 5  
+  tell ["Multiplying 3 and 5"]
+  return (a*b) 
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 =    do
+      tell ["Finished with a " ++ show a]
+      return a
+  | otherwise = do
+      tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      gcd' b (a `mod` b)
